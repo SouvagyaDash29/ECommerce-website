00b82fd2c94b54d8d6f189c4522fbb9cccadbfe4
SouvagyaDash29 <souvagyadash1999@gmail.com>
2024-12-30 18:57:38 +0530

    just add a dropdown menu in shop page

--- frontend/src/components/Client/Pages/Shop.jsx
+++ frontend/src/components/Client/Pages/Shop.jsx
@@ -3,14 +3,15 @@ import ShopImg from "../../../Assets/Image Placeholder header.png"
 import Dropdown from '../../../Common/Dropdown';
 const Shop = () => {
   return (
-    <div>
-      <div class="bg-hero-pattern bg-no-repeat bg-cover bg-top w-[80%] h-[25rem] mx-auto flex flex-col gap-4 justify-center items-center">
+    <div className='w-[80%] mx-auto'>
+      <div class="bg-hero-pattern bg-no-repeat bg-cover bg-top  h-[25rem] flex flex-col gap-4 justify-center items-center">
         <span className='font-poppins'>{`Home > Shop `}</span>
         <h1 className='font-poppins lg:text-5xl font-medium'>Shop Page</h1>
         <span className='font-inter lg:text-xl'>Let’s design the place you always imagined.</span>
       </div>
 
-      <div className="">
+      <div className="flex gap-4">
+        <Dropdown />
         <Dropdown />
       </div>
 
